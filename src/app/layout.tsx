a017e3d6433877467a644fe1c0d7d3f45351506b
zuicy99 <k.zuicy@gmail.com>
2024-03-27 11:02:00 +0900

    env 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
 export const metadata: Metadata = {
-  title: '전국 보건소 위치 안내',
+  title: '전국 보건소 위치 안내 앱',
   description: '전국 보건소 위치 안내 서비스 앱',
 };
 
